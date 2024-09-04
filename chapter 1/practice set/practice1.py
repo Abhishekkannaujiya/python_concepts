c0bcba8e9d5b934abab246fc6a031d768f7abb30
abhishek <abhishekkannaujiya824@gmail.com>
2024-09-04 15:12:13 +0530

    Update practice1.py

--- chapter 1/practice set/practice1.py
+++ chapter 1/practice set/practice1.py
@@ -1,1 +1,8 @@
+# print twinkle twinkle little star
 
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are")
